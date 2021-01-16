0ffefb8ccab77060bf33de948cd7b61cbba41ac7
MartaLebre <55530854+MartaLebre@users.noreply.github.com>
2021-01-16 16:56:21 +0000

    api update

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Dbz41tR2IAxfN8_Oaybv4hSurhdItiOt',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
